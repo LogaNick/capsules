2e2502e020c030d7cd71063d43ec175c7140ef9c
Jeff//ZEN_BOOK <jeffreyjfillingham@gmail.com>
2018-10-05 16:27:46 -0300

    Unrolled dynamic routing that can pass through input from a lower capsule layer.

--- notebook_setup.py
+++ notebook_setup.py
@@ -77,11 +77,89 @@ def dynamic_routing(U_j_given_i, iterations, input_capsule_layer, output_capsule
     
     return V
     
+# GOAL: Pass data from input, through the capsules without changing it, then
+#       use a fully connected layer to subtract the second capsule (input)
+#       from the first
+#
+# Network:
+#       Input: [x1, y1, x2, y2]
+#       Output: [x2-x1, y2-1]
+#
 
-u1 = np.matrix([1, 1]).T
-u2 = np.matrix([2, 2]).T
 
-squash(u1)
+u1 = np.array([1, 2])
+u2 = np.array([2, 3])
 
+# Weight matrices W_i_j
+W_1_1 = np.eye(2)
+W_1_2 = np.zeros((2, 2))
+
+W_2_1 = np.zeros((2, 2))
+W_2_2 = np.eye(2)#np.zeros((2, 2)) #np.identity(2)
+
+# u_j_given_i = W_ij * u_i
+u_1_given_1 = np.matmul(W_1_1, u1)
+u_1_given_2 = np.matmul(W_2_1, u2)
+
+u_2_given_1 = np.matmul(W_1_2, u1)
+u_2_given_2 = np.matmul(W_2_2, u2)
+
+# Dynamic routing unrolled...
+# b_ij = 0
+b_1_1 = 0
+b_1_2 = 0
+b_2_1 = 0
+b_2_2 = 0
+
+
+# r iterations
+r = 3
+for _ in range(r):
+    print("iteration {}".format(_))
+    print("b_1: [{}, {}]".format(b_1_1, b_1_2))
+    print("b_2: [{}, {}]".format(b_2_1, b_2_2))
+    # c_i = softmax(b_i)
+    # b_1
+    denominator = np.exp(b_1_1) + np.exp(b_1_2)    
+    c_1_1 = np.exp(b_1_1) / denominator
+    c_1_2 = np.exp(b_1_2) / denominator
+    print("c_1: [{}, {}]".format(c_1_1, c_1_2))
+    
+    # b_2
+    denominator = np.exp(b_2_1) + np.exp(b_2_2)
+    c_2_1 = np.exp(b_2_1) / denominator
+    c_2_2 = np.exp(b_2_2) / denominator
+    print("c_2: [{}, {}]".format(c_2_1, c_2_2))
+    
+    # This is where it gets confusing. u_1_given_2 is a vector/matrix (2, 1)
+    # for all capsule j in layer (l + 1): s_j <- sum_i(c_ij * u_j_given_i)
+    s_1 = c_1_1 * u_1_given_1 + c_2_1 * u_1_given_2 
+    s_2 = c_1_2 * u_2_given_1 + c_2_2 * u_2_given_2
+    print("s_1: {}".format(s_1.T))
+    print("s_2: {}".format(s_2.T))
+    
+    
+    # for all capsule j in layer (l + 1): v_j <- squash(s_j)
+    v_1 = s_1 #squash(s_1)
+    v_2 = s_2 #squash(s_2)
+    print("v_1: {}".format(v_1.T))
+    print("v_2: {}".format(v_2.T))
+    
+    # for all capsule i in layer l and capsule j in layer (l + 1): b_ij <- b_ij + u_j_given_i dot v_j
+    b_1_1 = b_1_1 + np.asscalar(np.dot(u_1_given_1, v_1))
+    b_1_2 = b_1_2 + np.asscalar(np.dot(u_2_given_1, v_2))
+    b_2_1 = b_2_1 + np.asscalar(np.dot(u_1_given_2, v_1))
+    b_2_2 = b_2_2 + np.asscalar(np.dot(u_2_given_2, v_2))
+    
+
+
+
+# Output layer with weights 
+# TODO: Fully connected layer
+
+
+
+"""
 U_j_give_i = np.concatenate([u1, u2], axis=1).T
 V = dynamic_routing(U_j_give_i, 3, U_j_give_i, U_j_give_i)
+"""
